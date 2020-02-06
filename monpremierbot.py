4b4208acbbd415b10aadf3bfe140155a5b76d32d
ballakeita <ballakei070@gmail.com>
2020-02-06 11:05:47 +0100

    ma premiere version de bot

---
+++ monpremierbot.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+salutation = ['Salut','Azul','inisokoma','inichou','baye baye !!','mbifai', 'bonjour', 'Bonsoir']
+question = input("Bonjour je suis votre premier bot, veillez donner un mot")
+if (question.lower() == "salut"):
+    print("salut mon ami")
+elif (question.lower() == "bonjour"):
+    print("salut mon ami")
+elif (question.lower() == "bonsoir"):
+    print("salut mon ami")
+else:
+    print("pas toi mon ami")
