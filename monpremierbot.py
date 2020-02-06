928e3527aebd9df1881e802bf371e8621196d59b
ballakeita <ballakei070@gmail.com>
2020-02-06 11:30:10 +0100

    2eme mise en jour

--- monpremierbot.py
+++ monpremierbot.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
+#salutation = ['Salut','Azul','inisokoma','inichou','baye baye !!','mbifai', 'bonjour', 'Bonsoir']
 
-salutation = ['Salut','Azul','inisokoma','inichou','baye baye !!','mbifai', 'bonjour', 'Bonsoir']
-question = input("Bonjour je suis votre premier bot, veillez donner un mot")
-if (question.lower() == "salut"):
-    print("salut mon ami")
-elif (question.lower() == "bonjour"):
-    print("salut mon ami")
-elif (question.lower() == "bonsoir"):
-    print("salut mon ami")
+question = input("Bonjour je suis votre premier bot, veillez donner un mot\n")
+proposition = ["Salut","bonjour","bonsoir"]
+if (question.lower() == in proposition):
+   print("salut mon ami")
 else:
-    print("pas toi mon ami")
+   print("je ne comprends pas")
